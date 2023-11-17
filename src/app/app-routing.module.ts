9529b7fddb9be38e6b55e9b9ad7b14f966896e4b
Ubiratan Leitão Mourão <ubiratan.mourao@solutis.com.br>
2023-11-17 14:03:14 -0300

    Criacao da aplicacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {CalculadoraPessoasComponent} from "./calculadora-pessoas/calculadora-pessoas.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'calculadora-pessoas',
+  component: CalculadoraPessoasComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true}),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from "primeng/button";
+import { CalculadoraPessoasComponent } from './calculadora-pessoas/calculadora-pessoas.component';
+import { MenuComponent } from './menu/menu.component';
+import {DividerModule} from "primeng/divider";
+import {BadgeModule} from "primeng/badge";
+import {TieredMenuModule} from "primeng/tieredmenu";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraPessoasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    DividerModule,
+    BadgeModule,
+    TieredMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
