e2bf7194554bc224e8f46aa1e2f0b60c7ab0fb05
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2026-03-12 18:38:31 -0300

    Correção dia de culto

--- src/app/model/cultos-enum.ts
+++ src/app/model/cultos-enum.ts
@@ -1,11 +1,11 @@
 export enum DiaDaSemana {
+  Domingo = 0,
   Segunda = 1,
   Terca = 2,
   Quarta = 3,
   Quinta = 4,
   Sexta = 5,
-  Sabado = 6,
-  Domingo = 7,
+  Sabado = 6
 }
 export const CultosDiaSemana = new Map<string, string>([
   ['Quarta', 'Quarta de Milagres'],
